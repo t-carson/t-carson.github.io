dd1582ae2303291a341672218f832103e1b4b8bc
Carson <1519153391@qq.com>
2022-11-22 22:30:49 +0800

    fix:去掉多余btn并且新增其他按钮

--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -47,7 +47,9 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
-        <el-button type="primary" native-type="submit">保存1</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存2</el-button>
       </el-form-item>
     </el-form>
     <!-- 表单结束 -->
